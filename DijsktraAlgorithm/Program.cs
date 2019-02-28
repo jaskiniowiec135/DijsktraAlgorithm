9508741c6d1f6c6dd8d260825ca97477abab94ed
jaskiniowiec135 <34076765+jaskiniowiec135@users.noreply.github.com>
2019-02-28 13:51:48 +0100

    Almost working version
    Need a break for a walk

---
+++ DijsktraAlgorithm/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijsktraAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] graph = new int[5,5] { {0,5,10,0,0},
+                                          {0,0,3,9,2},
+                                          {0,2,0,0,1},
+                                          {0,0,0,0,4},
+                                          {7,0,0,6,0}};
+            int startIndex = 0;
+            int[] result = findPath(startIndex, graph);
+
+            Console.WriteLine("Odległość od wierzchołka {0} do danych wierzchołków wynosi: ", startIndex);
+            for (int i = 1; i < result.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}",i,result[i]);
+            }
+
+        }
+
+        static int[] findPath(int startIndex, int[,] graph)
+        {
+            int[] result = new int[graph.GetLength(0)];
+            List<int> knownVerticles = new List<int>();
+            int currentVerticle = startIndex;
+
+            List<int> verticles = new List<int>();
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                verticles.Add(i);
+            }
+
+            knownVerticles.Add(startIndex);
+            verticles.Remove(startIndex);
+            ratePathFromVerticle(graph, result, startIndex);
+
+            while (verticles.Count > 0)
+            {
+                List<int> tmp = new List<int>();
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if ((int)graph.GetValue(currentVerticle,i) > 0)
+                    {
+                        tmp.Add((int)graph.GetValue(currentVerticle, i));
+                    }
+                }
+
+                for (int i = currentVerticle; i == currentVerticle; i--)
+                {
+                    for (int j = 0; j < graph.GetLength(0); j++)
+                    {
+                        if (graph[i, j] == tmp.Min())
+                        {
+                            knownVerticles.Add(j);
+                            verticles.Remove(j);
+                            currentVerticle = j;
+                            break;
+                        }
+                    }
+                }
+
+                ratePathFromVerticle(graph, result, currentVerticle);
+            }
+            return result;
+        }
+
+        static void ratePathFromVerticle(int[,] graph, int[] pathRates, int currVert)
+        {
+            for (int i = currVert; i == currVert; i--)
+            {
+                for (int j = 1; j < graph.GetLength(0); j++)
+                {
+                    if(graph[i,j] > 0)
+                    {
+                        if(currVert != 0)
+                        {
+                            if(graph[i,j] + pathRates[currVert] < pathRates[j]||pathRates[j] == 0)
+                            {
+                                pathRates[j] = graph[i, j] + pathRates[currVert];
+                            }
+                        }
+                        else
+                        {
+                            pathRates[j] = graph[i, j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DijsktraAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("DijsktraAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DijsktraAlgorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("5d597141-67ad-4691-9d4d-f9ffdf335e25")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
