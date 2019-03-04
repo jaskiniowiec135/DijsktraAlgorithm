a0b915216f9e98fef5b06b49ff9945356562b861
jaskiniowiec135 <34076765+jaskiniowiec135@users.noreply.github.com>
2019-03-04 20:58:26 +0100

    Added lastVerticle
    It gives the shortest path from any verticle and throw it to the console.

--- DijsktraAlgorithm/Program.cs
+++ DijsktraAlgorithm/Program.cs
@@ -15,11 +15,21 @@ namespace DijsktraAlgorithm
                                           {0,2,0,1,0},
                                           {0,0,0,0,4},
                                           {7,0,0,6,0}};
-            int startIndex = 0;
-            int[] result = findPath(startIndex, graph);
+
+            int[,] graphToo = new int[9, 9] { {0,4,9,0,7,0,0,10,0},
+                                              {0,0,4,5,0,4,0,0,0},
+                                              {0,0,0,0,0,8,0,0,0},
+                                              {3,0,0,0,2,0,4,0,0},
+                                              {0,0,0,0,0,7,4,5,0},
+                                              {0,0,6,0,0,0,0,0,8},
+                                              {0,0,0,0,0,0,0,2,0},
+                                              {0,0,0,0,0,3,5,0,6},
+                                              {0,0,12,0,0,0,0,0,0} };
+            int startIndex = 1;
+            int[] result = findPath(startIndex, graphToo);
 
             Console.WriteLine("Odległość od wierzchołka {0} do danych wierzchołków wynosi: ", startIndex);
-            for (int i = 1; i < result.Length; i++)
+            for (int i = 0; i < result.Length; i++)
             {
                 Console.WriteLine("{0}: {1}",i,result[i]);
             }
@@ -31,6 +41,7 @@ namespace DijsktraAlgorithm
         static int[] findPath(int startIndex, int[,] graph)
         {
             int[] result = new int[graph.GetLength(0)];
+            int[] lastVertex = new int[graph.GetLength(0)];
             List<int> knownVerticles = new List<int>();
             int currentVertex = startIndex, nextVertex = 0;
 
@@ -46,33 +57,43 @@ namespace DijsktraAlgorithm
 
                 for (int i = currentVertex; i == currentVertex; i--)
                 {
-                    for (int j = 1; j < graph.GetLength(0); j++)
+                    nextVertex = -1;
+                    for (int j = 0; j < graph.GetLength(0); j++)
                     {
-                        if (graph[i, j] > 0 && graph[i, j] + result[currentVertex] < min)
+                        if (graph[i, j] > 0 && j != startIndex)
                         {
-                            min = graph[i, j] + result[currentVertex];
-                            nextVertex = j;
+                            if (result[j] == 0)
+                            {
+                                result[j] = result[currentVertex] + graph[i, j];
+                            }
+                            else if (result[currentVertex] + graph[i, j] < result[j])
+                            {
+                                result[j] = result[currentVertex] + graph[i, j];
+                                nextVertex = j;
+                                lastVertex[j] = currentVertex;
+                            }
+
+
+                            //TODO rozbić na mniejsze ify + warunek z b do e do a
+                            if (result[j] < min && j != lastVertex[currentVertex] && verticles.IndexOf(j) != -1)
+                            {
+                                min = graph[i, j] + result[currentVertex];
+                                if (min <= result[j])
+                                {
+                                    nextVertex = j;
+                                    lastVertex[j] = currentVertex;
+                                }
+                            }
                         }
                     }
 
-                    if (currentVertex != 0)
+                    if (nextVertex == -1)
                     {
-                        if(verticles.IndexOf(currentVertex) == -1)
-                        {
-                            nextVertex = verticles[0];
-                        }
+                        nextVertex = lastVertex[i];
                         knownVerticles.Add(currentVertex);
                         verticles.Remove(currentVertex);
-                        i = 0;
-                    }
-                    else
-                    {
-                        knownVerticles.Add(0);
-                        verticles.Remove(0);
                     }
                 }
-
-                ratePathFromVerticle(graph, result, currentVertex);
                 currentVertex = nextVertex;
             }
 
